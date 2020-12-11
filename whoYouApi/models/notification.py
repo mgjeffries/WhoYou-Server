439ce769fd47b6d03d4dff53e1c06a252d4034ae
mgjeffries <gib.jeffries@gmail.com>
2020-12-11 13:59:48 -0600

    add todo reminder in notification model

--- whoYouApi/models/notification.py
+++ whoYouApi/models/notification.py
@@ -5,3 +5,4 @@ class Notification(models.Model):
     """Database whoYou notification Model"""
     target_user = models.ForeignKey("WhoYouUser", on_delete=models.CASCADE)
     content = models.CharField(max_length=500)
+    # TODO: add a property to tell if the user has dismissed the notification

a5f1b34b76237d960975c527698f9b1547443f1b
mgjeffries <gib.jeffries@gmail.com>
2020-12-15 11:16:36 -0600

    add retrieve function to content

--- whoYouApi/views/content.py
+++ whoYouApi/views/content.py
@@ -6,6 +6,7 @@ from whoYouApi.models import WhoYouUser, Content
 from django.utils import timezone
 from rest_framework import serializers
 from rest_framework.response import Response
+from rest_framework import status
 
 
 class ContentViewSet(ViewSet):
@@ -46,20 +47,61 @@ class ContentViewSet(ViewSet):
         # Censor out values that the user shouldn't be able to access
         censored_content_objects = []
         for content_object in content_objects:
-            isRequesterContentOwner = WhoYouUser.objects.get(user=request.auth.user) == content_object.owner
-            if content_object.is_public or isRequesterContentOwner:
-                censored_content_objects.append(content_object)
-            #TODO: check if the requester has a valid view request for this content
-            else:
-                content_object.value = "restricted value"
-                censored_content_objects.append(content_object)
+            censored_content = censorContent(content_object, request.auth.user)
+            censored_content_objects.append(censored_content)
 
         serializer = ContentSerializer(
-            content_objects, many=True, context={'request': request}
+            censored_content_objects, many=True, context={'request': request}
         )
         return Response(serializer.data)
 
 
+    def retrieve(self, request, pk=None):
+        """Handle GET request for single content instance
+        Returns:
+            Response JSON serialized content instance
+        """
+        content = Content.objects.get(pk=pk)
+        censoredContent = censorContent(content, request.auth.user)
+        serializer = ContentSerializer(content, context={'request': request})
+        return Response(serializer.data)
+        
+    def update(self, request, pk=None):
+        """ Handle an PUT request for content
+        Returns:
+            Response code
+        """
+
+        # Find the post being updated based on it's primary key
+        content = Content.objects.get(pk=pk)
+
+        #Prevent non-admin users from modifying other user's posts
+        requester = WhoYouUser.objects.get(user=request.auth.user)
+        if requester != content.owner:
+            return Response({"message": "Permission denied"}, status=status.HTTP_401_UNAUTHORIZED)
+
+        # save basic (non-associated) properties
+        content.owner = requester
+        field_type = FieldType.objects.get(pk=request.data["field_type"])
+        content.field_type = field_type
+        content.value = request.data["value"]
+        content.is_public = request.data["is_public"] == "true"
+        content.verification_time = timezone.now()
+
+        # save content object
+        content.save()
+
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+
+def censorContent(content_object, requestingUser):
+    isRequesterContentOwner = WhoYouUser.objects.get(user=requestingUser) == content_object.owner
+    if content_object.is_public or isRequesterContentOwner:
+    #TODO: check if the requester has a valid view request for this content
+        return content_object
+    else:
+        content_object.value = "restricted value"
+        return content_object
 
 class ContentSerializer(serializers.ModelSerializer):
     """Serializer for Content"""     

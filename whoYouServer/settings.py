545a6c9b7c2092c9c5000e42819e3dbd0f1f423d
mgjeffries <gib.jeffries@gmail.com>
2021-01-08 09:53:36 -0600

    remove path from cors whitelist

--- whoYouServer/settings.py
+++ whoYouServer/settings.py
@@ -56,7 +56,7 @@ REST_FRAMEWORK = {
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'https://whoyou-client.herokuapp.com/'
+    'whoyou-client.herokuapp.com'
 )
 
 
